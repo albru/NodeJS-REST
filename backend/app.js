23022f4eae86662ebe442699a8e9b3557a4a2538
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2020-02-11 20:07:26 +0300

    socket io

--- backend/app.js
+++ backend/app.js
@@ -61,9 +61,13 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    'key'
+
   )
   .then(result => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
   })
   .catch(err => console.log(err));
